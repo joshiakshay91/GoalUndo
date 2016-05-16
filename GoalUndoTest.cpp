68a946d07c3ec090bc9e2b39c79f9670d4b2dc94
joshiakshay91 <joshiaks91@gmail.com>
2016-05-15 19:26:14 -0700

    add ops full

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,8 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
-
+#include<iostream>
+using namespace std;
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -13,8 +14,93 @@ class GoalUndoTest : public ::testing::Test
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
-//Sanity test
+//Sanity check test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+//Just check empty goal
+TEST(GoalUndoTest, GetEmptyGoal)
+{
+  GoalUndo G;
+  std::string out="";
+  ASSERT_EQ(out,G.getGoal());
+}
+
+
+//Just check goal there or not
+TEST(GoalUndoTest, GetOneVal)
+{
+  GoalUndo G;
+  G.addOperation("GoalNameTest","operationNameTest");
+  std::string out="GoalNameTest";
+  ASSERT_EQ(out,G.getGoal());
+}
+
+
+//Just check empty goal is not added
+TEST(GoalUndoTest, TryEmptyGoal)
+{
+  GoalUndo G;
+  G.addOperation("","operationNameTest");
+  std::string out="";//to check this I will see wheter operation is also empty
+  ASSERT_EQ(out,G.getOperations());
+}
+
+
+//Just check empty operation is not added
+TEST(GoalUndoTest, TryEmptyOperation)
+{
+  GoalUndo G;
+  G.addOperation("GoalNameTest","");
+  std::string out="";//to check this I will see wheter operation is also empty
+  ASSERT_EQ(out,G.getGoal());
+}
+
+
+//add operations in here
+TEST(GoalUndoTest, AddingPrintingOps)
+{
+  GoalUndo G;
+  bool flag=false;
+  G.addOperation("GoalNameTest","Test1");
+  G.addOperation("Test2");
+  G.addOperation("Test3");
+  if(G.getOperations()=="Test1 Test2 Test3")
+  {
+    flag=true;
+  }
+  ASSERT_TRUE(flag);
+}
+
+//NO goals exist I add operation
+TEST(GoalUndoTest, NgoalsAddOps)
+{
+  bool flag=false;
+  GoalUndo G;
+  G.addOperation("Name Of operation n goal");
+  if(!(G.getGoal().compare("Name Of operation n goal")))
+  {
+    flag=true;
+  }
+  ASSERT_TRUE(flag);
+}
+
+//undo operation working check
+TEST(GoalUndoTest,UndoWorkingCheck)
+{
+  GoalUndo G;
+  bool flag=false;
+  G.addOperation("GoalNameTest","Test1");
+  G.addOperation("Test2");
+  G.addOperation("Test3");
+  G.undoOperation("Test3");
+  if(!(G.getOperations().compare("Test1 Test2")))
+  {
+    flag=true;
+  }
+  ASSERT_TRUE(flag);
+
+
 }
